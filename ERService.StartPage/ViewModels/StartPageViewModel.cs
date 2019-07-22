094083279e709ec07a0b292579ac6cf766070be3
Radek Kurek <r.kurek@sputniksoftware.com>
2019-07-22 09:05:03 +0200

    StartPage - Added Main Navigation Items

--- ERService.StartPage/ViewModels/StartPageViewModel.cs
+++ ERService.StartPage/ViewModels/StartPageViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using ERService.Infrastructure.Constants;
+using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Regions;
 
@@ -7,19 +8,51 @@ namespace ERService.StartPage.ViewModels
 {
     public class StartPageViewModel : BindableBase, INavigationAware, IConfirmNavigationRequest, IRegionMemberLifetime
     {
-        private string _message;
-
-        public string Message
-        {
-            get { return _message; }
-            set { SetProperty(ref _message, value); }
-        }
-
         public bool KeepAlive => false;
+
+        private IRegionManager _regionManager;
+
+        public DelegateCommand OrdersCommand { get; private set; }
+        public DelegateCommand CustomersCommand { get; private set; }
+        public DelegateCommand SettingsCommand { get; private set; }
+        public DelegateCommand AddOrderCommand { get; private set; }
 
         public StartPageViewModel(IRegionManager regionManager)
         {
+            _regionManager = regionManager;
 
+            OrdersCommand = new DelegateCommand(OnOrdersCommandExecute);
+            CustomersCommand = new DelegateCommand(OnCustomersCommandExecute);
+            SettingsCommand = new DelegateCommand(OnSettingsCommandExecute);
+            AddOrderCommand = new DelegateCommand(OnAddOrderExecute);
+        }
+
+        private void OnAddOrderExecute()
+        {
+            var parameters = new NavigationParameters();
+            parameters.Add("ID", Guid.Empty);
+            parameters.Add("Wizard", true);
+            parameters.Add("ViewFullName", ViewNames.CustomerView);
+
+            _regionManager.RequestNavigate(RegionNames.ContentRegion, new Uri(ViewNames.CustomerView + parameters, UriKind.Relative));
+        }
+
+        private void OnSettingsCommandExecute()
+        {
+            _regionManager.Regions[RegionNames.ContentRegion].RemoveAll(); 
+            _regionManager.RequestNavigate(RegionNames.ContentRegion, ViewNames.SettingsView);
+        }
+
+        private void OnCustomersCommandExecute()
+        {
+            _regionManager.Regions[RegionNames.ContentRegion].RemoveAll(); 
+            _regionManager.RequestNavigate(RegionNames.ContentRegion, ViewNames.CustomerListView);
+        }
+
+        private void OnOrdersCommandExecute()
+        {
+            _regionManager.Regions[RegionNames.ContentRegion].RemoveAll(); 
+            _regionManager.RequestNavigate(RegionNames.ContentRegion, ViewNames.OrderListView);
         }
 
         public void ConfirmNavigationRequest(NavigationContext navigationContext, Action<bool> continuationCallback)
@@ -29,7 +62,7 @@ namespace ERService.StartPage.ViewModels
 
         public void OnNavigatedTo(NavigationContext navigationContext)
         {
-            Message = "View StarPage from your Prism Module";
+            
         }
 
         public bool IsNavigationTarget(NavigationContext navigationContext)
